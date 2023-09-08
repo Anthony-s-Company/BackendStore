0040eaa315258683d45745a2310a0a26e32d45bc
Anthony Ruiz <yobakaj@gmail.com>
2023-09-08 11:49:50 -0400

    TODO: fix error when seed DB

--- db/models/Store.js
+++ db/models/Store.js
@@ -21,55 +21,54 @@ async function createTables() {
   try {
     console.log("Building All Tables...");
     await client.query(`
+      CREATE TABLE user (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        name VARCHAR(255),
+        lastname VARCHAR(255),
+        address1 VARCHAR(255),
+        address2 VARCHAR(255),
+        city VARCHAR(50),
+        state VARCHAR(50),
+        zip VARCHAR(25),
+        county VARCHAR(50),
+        active BOOLEAN DEFAULT true,
+        "imgUrl" VARCHAR(255) DEFAULT 'https://i.imgur.com/An7G9J1b.jpg'
+        );
 
-        CREATE TABLE user (
-          id SERIAL PRIMARY KEY,
-          email VARCHAR(255) UNIQUE NOT NULL,
-          username VARCHAR(255) UNIQUE NOT NULL,
-          password VARCHAR(255) NOT NULL,
-          name VARCHAR(255),
-          lastname VARCHAR(255),
-          address1 VARCHAR(255),
-          address2 VARCHAR(255),
-          city VARCHAR(50),
-          state VARCHAR(50),
-          zip VARCHAR(25),
-          county VARCHAR(50),
-          active BOOLEAN DEFAULT true,
-          "imgUrl" VARCHAR(255) DEFAULT 'https://i.imgur.com/An7G9J1b.jpg'
-          );
+      CREATE TABLE category (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT
+        );
 
-        CREATE TABLE category (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) UNIQUE NOT NULL,
-          description TEXT
-          );
+      CREATE TABLE product (
+        id SERIAL PRIMARY KEY,
+        category_id INTEGER NOT NULL,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT,
+        imgUrl VARCHAR(255) DEFAULT 'https://i.imgur.com/rS1Nzax.png',
+        stock INTEGER,
+        price FLOAT,
+        vendor VARCHAR(255)
+        );
 
-        CREATE TABLE product (
-          id SERIAL PRIMARY KEY,
-          category_id INTEGER NOT NULL,
-          name VARCHAR(255) UNIQUE NOT NULL,
-          description TEXT,
-          imgUrl VARCHAR(255) DEFAULT 'https://i.imgur.com/rS1Nzax.png',
-          stock INTEGER,
-          price FLOAT,
-          vendor VARCHAR(255)
-          );
+      CREATE TABLE item (
+        id SERIAL PRIMARY KEY,
+        product_id INTEGER NOT NULL,
+        qty INTEGER,
+        price FLOAT
+        );
 
-        CREATE TABLE item (
-          id SERIAL PRIMARY KEY,
-          product_id INTEGER NOT NULL,
-          qty INTEGER,
-          price FLOAT
-          );
-
-        CREATE TABLE order (
-          id SERIAL PRIMARY KEY,
-          item_id INTEGER NOT NULL,
-          status VARCHAR(50),
-          total FLOAT
-          );
-      `);
+      CREATE TABLE order (
+        id SERIAL PRIMARY KEY,
+        item_id INTEGER NOT NULL,
+        status VARCHAR(50),
+        total FLOAT
+        );
+    `);
   } catch (error) {
     throw error;
   }
--- db/seed/data.js
+++ db/seed/data.js
@@ -1,24 +1,29 @@
-import { faker } from '@faker-js/faker';
+const { faker } = require("@faker-js/faker");
 
-export const products = []
-export const categories = []
+const products = [];
+const categories = [];
 
 for (let i = 0; i < 50; i++) {
   products.push({
     category_id: Math.floor(Math.random() * 10) + 1,
     name: faker.commerce.productName(),
-    description: faker.commerceproductDescription(),
-    imgUrl: faker.image.urlLoremFlickr({ category: 'technics' }),
+    description: faker.commerceproductDescription,
+    imgUrl: faker.image.urlLoremFlickr({ category: "technics" }),
     stock: faker.number.int({ max: 100 }),
     price: faker.number.float({ max: 500 }),
-    vendor: faker.company.name()
-  })
+    vendor: faker.company.name(),
+  });
 }
 
 for (let i = 0; i < 10; i++) {
   categories.push({
     product_id: Math.floor(Math.random() * 50) + 1,
     name: faker.commerce.product(),
-    description: faker.commerceproductDescription()
-  })
+    description: faker.commerceproductDescription,
+  });
 }
+
+module.exports = {
+  products,
+  categories,
+};
